b4c20e947b81317c59bfd6e5ac3d9d224ab9aa60
Sushan Paudyal <cto@onlinezeal.com>
2019-01-07 15:45:23 +0545

    Products Detail More Edit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -71,44 +71,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-3">
-                    <div class="left-sidebar">
-                        <h2>Category</h2>
-                        <div class="panel-group category-products" id="accordian"><!--category-productsr-->
-
-                            <div class="panel panel-default">
-                                @foreach($categories as $cat)
-                                   @if($cat->status == 1)
-                                    <div class="panel-heading">
-                                        <h4 class="panel-title">
-                                            <a data-toggle="collapse" data-parent="#{{$cat->id}}" href="#{{$cat->id}}">
-                                                <span class="badge pull-right"><i class="fa fa-plus"></i></span>
-                                                {{ $cat->name }}
-                                            </a>
-                                        </h4>
-                                    </div>
-                                    <div id="{{$cat->id}}" class="panel-collapse collapse">
-                                        <div class="panel-body">
-                                            <ul>
-                                                @foreach($cat->categories as $subcat)
-                                                    @if($subcat->status === 1)
-                                                    <li><a href="{{asset('/products/'.$subcat->url)}}"> {{$subcat->name}} </a></li>
-                                                    @endif
-                                                @endforeach
-
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    @endif
-                                @endforeach
-                            </div>
-
-
-                        </div><!--/category-products-->
-
-
-
-
-                    </div>
+                    @include('layouts.frontLayout.front_sidebar')
                 </div>
 
                 <div class="col-sm-9 padding-right">
---
+++ resources/views/layouts/frontLayout/front_sidebar.blade.php
@@ -0,0 +1,33 @@
+<div class="left-sidebar">
+    <h2>Category</h2>
+    <div class="panel-group category-products" id="accordian"><!--category-productsr-->
+        <div class="panel panel-default">
+            @foreach($categories as $cat)
+
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#{{$cat->id}}" href="#{{$cat->id}}">
+                            <span class="badge pull-right"><i class="fa fa-plus"></i></span>
+                            {{ $cat->name }}
+                        </a>
+                    </h4>
+                </div>
+                <div id="{{$cat->id}}" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <ul>
+                            @foreach($cat->categories as $subcat)
+                                <li><a href="{{asset('/products/'.$subcat->url)}}"> {{$subcat->name}} </a></li>
+
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+    </div><!--/category-products-->
+
+
+
+
+</div>
